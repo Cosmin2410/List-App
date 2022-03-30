ed0896d562286baf605d3f1c2d087a7c2f171d00
cosmin <cosminnae00@yahoo.com>
2022-03-30 14:49:56 +0300

    functionalities added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const textValue = document.querySelector('.search');
+textValue.addEventListener('keyup', createRowEnter);
 
-const submitBtn = document
-  .querySelector('.submit')
-  .addEventListener('click', createRow);
+const submitBtn = document.querySelector('.submit');
+submitBtn.addEventListener('click', createRow);
 
 const listUl = document.querySelector('ul');
 listUl.addEventListener('click', cutThrough);
@@ -12,11 +12,49 @@ listUl.addEventListener('click', deleteItemsLi);
 
 const itemsLeft = document.querySelector('.items-left');
 
+const all = document.querySelector('.all');
+all.addEventListener('click', showAll);
+
+const active = document.querySelector('.active');
+active.addEventListener('click', showActive);
+
+const completed = document.querySelector('.completed');
+completed.addEventListener('click', showCompleted);
+
 const clearCompleted = document
   .querySelector('.clear-completed')
   .addEventListener('click', clearAllChecked);
 
-function createRow() {
+function createRowEnter(e) {
+  if (e.keyCode === 13) {
+    let ul = document.querySelector('ul');
+
+    let li = document.createElement('li');
+    ul.appendChild(li);
+
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList = 'check';
+
+    li.appendChild(checkBox);
+
+    let text = document.createElement('p');
+    text.classList = 'text-list';
+    text.innerHTML = textValue.value;
+    li.appendChild(text);
+
+    let deleteRow = document.createElement('img');
+    deleteRow.src = 'images/icon-cross.svg';
+    deleteRow.classList = 'delete';
+    li.appendChild(deleteRow);
+
+    itemsLeft.innerHTML++;
+
+    textValue.value = '';
+  }
+}
+
+function createRow(e) {
   let ul = document.querySelector('ul');
 
   let li = document.createElement('li');
@@ -39,6 +77,8 @@ function createRow() {
   li.appendChild(deleteRow);
 
   itemsLeft.innerHTML++;
+
+  textValue.value = '';
 }
 
 // line through the p or disable line through when chech or uncheck
@@ -81,6 +121,59 @@ function deleteItemsLi(e) {
 
 // Clear all the list items that are completed
 
-function clearAllChecked() {
-  console.log('a');
+function clearAllChecked(e) {
+  lis = document.querySelectorAll('.cut');
+  lis.forEach((li) => {
+    li.style.display = 'none';
+  });
+}
+
+// Buttons nav
+
+function showAll() {
+  all.style.color = 'rgb(53, 53, 236)';
+  active.style.color = ' rgb(175, 168, 168)';
+  completed.style.color = ' rgb(175, 168, 168)';
+
+  lisClear = document.querySelectorAll('li');
+  lisClear.forEach((liClear) => {
+    liClear.style.display = 'flex';
+  });
+
+  lis = document.querySelectorAll('.cut');
+  lis.forEach((li) => {
+    li.style.display = 'flex';
+  });
+}
+
+function showActive() {
+  active.style.color = 'rgb(53, 53, 236)';
+  all.style.color = 'rgb(175, 168, 168)';
+  completed.style.color = 'rgb(175, 168, 168)';
+
+  lisClear = document.querySelectorAll('li');
+  lisClear.forEach((liClear) => {
+    liClear.style.display = 'flex';
+  });
+
+  lis = document.querySelectorAll('.cut');
+  lis.forEach((li) => {
+    li.style.display = 'none';
+  });
+}
+
+function showCompleted() {
+  completed.style.color = 'rgb(53, 53, 236)';
+  active.style.color = ' rgb(175, 168, 168)';
+  all.style.color = ' rgb(175, 168, 168)';
+
+  lisClear = document.querySelectorAll('li');
+  lisClear.forEach((liClear) => {
+    liClear.style.display = 'none';
+  });
+
+  lis = document.querySelectorAll('.cut');
+  lis.forEach((li) => {
+    li.style.display = 'flex';
+  });
 }
